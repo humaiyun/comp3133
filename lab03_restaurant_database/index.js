87568d086893654ad49a369ce140c382a10da0cb
Humaiyun Uddin <uddin513@msn.com>
2023-02-02 14:37:59 -0500

    update server file to connect to mongo and port

--- lab03_restaurant_database/index.js
+++ lab03_restaurant_database/index.js
@@ -3,6 +3,33 @@ import mongoose from "mongoose";
 import dotenv from "dotenv"
 dotenv.config();
 
+import restaurantRoutes from "./routes/restaurant.js"
+
 const app = express();
+const PORT = process.env.SERVER_PORT;
+const DB_STRING = process.env.MONGODB_CONNECTION_STRING;
 
+import Restaurant from "./models/restaurant.js"
 
+app.use("/restaurants", restaurantRoutes);
+
+app.get("/", (req, res) => res.status(200).json({ message: "Server is running" }));
+
+const startServer = async () => {
+    try {
+        mongoose.connect(DB_STRING, { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        }).then(() => console.log("Connected to MongoDB"))
+          .catch((err) => console.log(err));
+
+        app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
+
+        
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startServer();
